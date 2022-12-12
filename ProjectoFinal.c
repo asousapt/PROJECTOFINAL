5cee0c68c71e3f89ab554acb9471084c30767f06
Antonio Sousa <ajfsousa@outlook.com>
2022-12-12 19:41:09 +0000

    ficheiro principal

---
+++ ProjectoFinal.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void menu() {
+    char escolha;
+    printf("*** Inscricao em Exames - MENU ***\n");
+    printf("A: Alunos\n");
+    printf("B: Alunos\n");
+
+    switch (escolha)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+}
+
+
+//Função Principal do programa
+void main(int argc, char** argv) { 
+  exercicio6c();
+}
