dc24b01809ddfadb56b9e2382c7af2fbeaaf3cbc
Antonio Sousa <ajfsousa@outlook.com>
2022-12-22 19:30:02 +0000

    test

--- UnidadesCurriculares.c
+++ UnidadesCurriculares.c
@@ -23,7 +23,7 @@ typedef struct
 //devolve uma string com os campos da linha lida
 STRING *Read_Split_Line_File(FILE *f, int *n_campos_lidos){
 	//separadores: carateres que fazem a separacao dos campos na linha
-	char *separadores = "\t\n", s[MAX_LINHA_FICHEIRO];
+	char *separadores = "|\n", s[MAX_LINHA_FICHEIRO];
 	*n_campos_lidos = 0;
 	if (!f) return NULL;
 	if ( fgets(s , MAX_LINHA_FICHEIRO , f) != NULL){	// fgets le uma linha do ficheiro de texto para a string s
@@ -60,21 +60,21 @@ void import_txt_uc(UNIDADECURRICULAR *uc, STRING *V){
 		V = Read_Split_Line_File(f, &n_campos_lidos);
 		if(V!=NULL){// caso consigamos ler alguma informacao
 
-		printf("%s\n",V[4]);
-			// uc[i].codigo=atoi(V[0]);
+		
+			uc[i].codigo=atoi(V[0]);
 			
-			// uc[i].descricao = malloc(sizeof(char)*(sizeof(V[1])));
-			// strcpy(uc[i].descricao, V[1]);
+			uc[i].descricao = malloc(sizeof(char)*(sizeof(V[1])));
+			strcpy(uc[i].descricao, V[1]);
 
-			// uc[i].curso = malloc(sizeof(char)*(sizeof(V[2])));
-			// strcpy(uc[i].curso, V[2]);
+			uc[i].curso = malloc(sizeof(char)*(sizeof(V[2])));
+			strcpy(uc[i].curso, V[2]);
 
-			// uc[i].ano=atoi(V[3]);
+			uc[i].ano=atoi(V[3]);
 
-			// uc[i].semestre=atoi(V[4]);
+			uc[i].semestre=atoi(V[4]);
 			
-			// uc[i].curso = malloc(sizeof(char)*(sizeof(V[2])));
-			// strcpy(uc[i].docente, V[5]);
+			uc[i].curso = malloc(sizeof(char)*(sizeof(V[2])));
+			strcpy(uc[i].docente, V[5]);
 
 			for (k = 0; k <n_campos_lidos; k++)
 				free (V[k]);
