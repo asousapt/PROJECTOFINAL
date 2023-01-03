7a643a7b26f6ac8e2f98fa2505a17e08e83b962b
ffonseca93 <120416178+ffonseca93@users.noreply.github.com>
2023-01-03 20:36:57 +0000

    Add files via upload

--- alunos.c
+++ alunos.c
@@ -1,53 +1,62 @@
-/*
- * Programa e C para manipular Alunos
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include "functions.h"
 
 #define MAX_CAR 300 
 #define STRING char *
 #define N_CAMPOS_MAX 100 
+#define MAX_LINHA_FICHEIRO 300
+#define MAX_UNIDADES_CURRICULARES 100
+#define MAX_CURSOS 20
 #define MAX_ALUNOS 3000
-#define MAX_REGIMES 10
+#define MAX_REGIMES 5
 
- //CRUD - Create,Read,Update,Delete
 
-//int contador_de_alunos = 0; //contador de alunos
-
+//passa para o vecto a informacao do ficheiro alunos.txt
 void import_txt_alunos(ALUNOS* alunos, STRING* V) {
-    int i = 0, k, n_campos_lidos;
+	int i = 0, k, n_campos_lidos;
 
-    FILE* f = fopen("alunos.txt", "r");
-    if (f == NULL) {
-        printf("Erro ao abrir o ficheiro alunos.txt");
-    }
-    do {
-        V = Read_Split_Line_File(f, &n_campos_lidos);
-        if (V != NULL) {
+	//Abre o ficheiro
+	FILE* f = fopen("alunos.txt", "r");
+	if (f == NULL) {
+		printf("Erro ao abrir ficheiro alunos.txt\n");
+		exit(1);
+	}
 
-            alunos[i].nome = (char*)malloc(sizeof(char) * (strlen(V[0]) + 1));
-            strcpy(alunos[i].nome, V[0]);
+	do {
+		V = Read_Split_Line_File(f, &n_campos_lidos);
+		if (V != NULL) {// caso consigamos ler alguma informacao			
 
-            alunos[i].regime = (char*)malloc(sizeof(char) * (strlen(V[1]) + 1));
-            strcpy(alunos[i].regime, V[1]);
+			alunos[i].nome = (char*)malloc(sizeof(char) * (strlen(V[0]) + 1));
+			strcpy(alunos[i].nome, V[0]);
 
-            alunos[i].ano_matricula = atoi(V[2]);
+			alunos[i].regime = (char*)malloc(sizeof(char) * (strlen(V[1]) + 5));
+			strcpy(alunos[i].regime, V[1]);
 
-            alunos[i].numero = atoi(V[3]);
+			alunos[i].ano_matricula = atoi(V[2]);
 
-            alunos[i].curso = (char*)malloc(sizeof(char) * (strlen(V[4]) + 1));
-            strcpy(alunos[i].nome, V[4]);
+			alunos[i].numero = atoi(V[3]);
 
-            //alunos[i].ocupado = 1;
-        }
-    } while (!feof(f));
+            alunos[i].curso = (char*)malloc(sizeof(char) * (strlen(V[4]) + 5));
+            strcpy(alunos[i].curso, V[4]);
 
-    fclose(f);
+            alunos[i].ocupado = 1;
+
+			for (k = 0; k < n_campos_lidos; k++)
+				free(V[k]);
+			free(V);
+			i++;
+		}
+	} while (!feof(f));
+
+	fclose(f);
 
 }
 
+//passa para o vetor a informacao do ficheiro regimes.txt
 void import_txt_regimes(REGIMES* regimes, STRING* V) {
     int i = 0, k, n_campos_lidos;
 
@@ -80,17 +89,7 @@ void import_txt_regimes(REGIMES* regimes, STRING* V) {
 
 }
 
-void listar_alunos(ALUNOS* alunos) {
-    int i;
-    printf("Nome|Regime|Ano Matricula|Numero|Curso\n");
-    for (i = 0; i < MAX_UNIDADES_CURRICULARES; i++)
-    {
-        if (alunos[i].ocupado == 1) {
-            printf("%s %s %d %d %s\n", alunos[i].nome, alunos[i].regime, alunos[i].ano_matricula, alunos[i].numero, alunos[i].curso);
-      }
-   }
-}
-
+//funca que lista os regimes
 void listar_regimes(REGIMES* regimes) {
     int i = 0;
 
@@ -98,193 +97,144 @@ void listar_regimes(REGIMES* regimes) {
     for (i = 0; i < MAX_REGIMES; i++)
     {
         if (regimes[i].ocupado == 1) {
-            printf("%-3d %s\n", i, regimes[i].regime);
+            printf("%d %s\n", i, regimes[i].regime);
         }
     }
 }
 
-int valida_regime_escolhido(REGIMES* regimes, int regime) {
-    int i = 0;
-
-    for (i = 0; i < MAX_REGIMES; i++)
-    {
-        if (regimes[i].ocupado == 1 && i == regime) {
-            return 1;
-        }
-    }
-
-    return 0;
+// funcao que lista os alunos 
+void listar_alunos(ALUNOS* alunos) {
+	int i;
+	printf("Nome | Regime | Ano Matricula | Numero | Curso\n");
+	for (i = 0; i < MAX_ALUNOS; i++)
+	{
+		if (alunos[i].ocupado == 1) {
+			printf("%s %s %d %d %s", alunos[i].nome, alunos[i].regime, alunos[i].ano_matricula, alunos[i].numero, alunos[i].curso);
+		}
+	}
+	printf("\n");
 }
-
-int valida_numero_aluno(ALUNOS* alunos, int numero_aluno) {
-    int i = 0;
-
-    for (i = 0; i < MAX_ALUNOS; i++)
-    {
-        if (alunos[i].numero == 1 && i == numero_aluno) {
-            return 0;
-        }
-    }
-
-    return 1;
-}
-
-int valida_ano(int ano_matricula) {
-    if (ano_matricula < 1 || ano_matricula > 3) {
-        return 0;
-    }
-    return 1;
-}
-
-int get_posicao_vect_alunos(ALUNOS* alunos) {
-    int i;
-    for (i = 0; i < MAX_ALUNOS; i++)
-    {
-        if ((alunos[i].ocupado == 0) && (i < MAX_ALUNOS)) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-int insere_aluno(ALUNOS* alunos, char* nome, char* regime, int ano_matricula, int numero, char* curso) {
-    int posicaoVazia = 0;
-    posicaoVazia = get_posicao_vect_alunos(alunos);
-    if (posicaoVazia == -1) {
-        return -1;
-    }
-
-    //insere a nova unidade curricular no vector 
-    alunos[posicaoVazia].nome = nome;
-    alunos[posicaoVazia].regime = regime;
-    alunos[posicaoVazia].ano_matricula = ano_matricula;
-    alunos[posicaoVazia].numero = numero;
-    alunos[posicaoVazia].curso = curso;
-    alunos[posicaoVazia].ocupado = 1;
-
-    return 1;
-}
-
-//Create
-void criar_aluno(ALUNOS* alunos, REGIMES* regimes, CURSO* cursos) {
-    //vamos pedir ao utilizador os dados para criar a nova unidade curricular
-                //declarecao de variaveis necessarias
-    char* nome;
-    char* regime;
-    int ano_matricula;
-    int numero_aluno;
-    int posicaoRegime;
-    char posicaoCurso;
-
-    //vamos pedir o nome do aluno 
-    do{
-        printf("Qual o nome do aluno?\n");
-        scanf("%s", &nome);
-   } while (strlen(nome) == 1);
-
-    //vamos pedir o regime 
-    do{
-        listar_regimes(regimes);
-        printf("Indique o regime da lista acima\n");
-        scanf("%d", &posicaoRegime);
-    } while (valida_regime_escolhido(regimes, posicaoRegime) == 0);
-
-    // introducao do ano de matricula
-    do {
-        printf("Indique o ano de matricula do aluno (1,2,3)\n");
-        scanf("%d", &ano_matricula);
-    } while (valida_ano(ano_matricula) == 0);
-
-    //introducao do numero do aluno
-    do {
-        printf("Indique o numero do aluno\n");
-        scanf("%d", &numero_aluno);
-    } while (valida_numero_aluno(alunos, numero_aluno) == 0);
-
-    // introducao do curso
-    do {
-        listar_cursos(cursos);
-        printf("Indique o curso do aluno de acordo com a lista acima\n");
-        scanf("%d", &posicaoCurso);
-    } while (valida_curso_escolhido(cursos, posicaoCurso));
-
-    //valida se a unidade curricular já existe no nosso vector
-    //No caso de retornar 1 a UC ja existe, caso contrario podemos inserir 
-    insere_aluno(alunos, nome, regimes[posicaoRegime].regime, ano_matricula, numero_aluno, cursos[posicaoCurso].codcurso); // == 1);
-    printf("Aluno inserido com sucesso!\n");
-    printf("\n");
-    }
 /*
-// Buscar Aluno no ficheiro.
-void buscar_aluno(char* numero_aluno) {
-    char word[100];
-    char line[100];
-
-    // Abre o arquivo para leitura
-    arquivo_aluno = fopen("alunos.txt", "r");
-    if (arquivo_aluno == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-    }
-    // Enquanto não chegar ao fim do arquivo, lê cada linha do arquivo
-    while (fgets(line, sizeof(line), arquivo_aluno) != NULL)
-    {
-        // Se a palavra for encontrada, imprime a linha
-        if (strstr(line, numero_aluno) != NULL)
-        {
-            printf("%s", line);
-        }
-    }
-    // Fecha o arquivo
-    fclose(arquivo_aluno);
+//verifica se a sala ja existe
+int valida_sala_existe_vector(SALAS* salas, char* nome_sala) {
+	int i = 0; 
+	for ( i = 0; i < MAX_UNIDADES_CURRICULARES; i++)
+	{
+		if (salas[i].ocupado == 1) {
+			
+			if (strcmp(salas[i].nome_sala, nome_sala) == 0) { 
+				return 1;
+			}	
+		}
+	}
+	return 0;
 }
 
-void ler_alunos() {
-    // Abre o arquivo para leitura
-    FILE* arquivo_aluno = fopen("alunos.txt", "r");
-    if (arquivo_aluno == NULL) {
-        printf("Erro ao abrir o arquivo\n");
-        return;
-    }
+//
+int get_posicao_vect_salas(SALAS* salas) {
+	int i;
+	for (i = 0; i < MAX_SALAS; i++)
+	{
+		if ((salas[i].ocupado == 0) && (i < MAX_SALAS)) {
+			return i;
+		}
+	}
+	return -1;
+}
 
-    // Lê os dados do arquivo e armazena na estrutura "aluno"
-    ALUNOS aluno;
-    while (fscanf(arquivo_aluno, "%d %s %s %d %s", &aluno.numero, aluno.nome, aluno.curso, &aluno.ano_matricula, aluno.regime) == 5) {
-        // Aqui você pode fazer alguma coisa com os dados lidos, como adicionar a um vetor de alunos, por exemplo
-        printf("Aluno: %s %s %s %d %s\n", aluno.nome, aluno.curso, aluno.regime, aluno.ano_matricula, aluno.numero);
-    }
+//Insere sala
+int insere_sala(SALAS* salas, char* codigo, char* nome_sala, int lotacao) {
+	int posicaoVazia = 0;
+	posicaoVazia = get_posicao_vect_salas(salas);
+	if (posicaoVazia == -1) {
+		return -1;
+	}
 
-    // Fecha o arquivo
-    fclose(arquivo_aluno);
+	//insere a nova sala no vector 
+	salas[posicaoVazia].codigo = codigo;
+	salas[posicaoVazia].nome_sala = nome_sala;
+	salas[posicaoVazia].lotacao = lotacao;
+	salas[posicaoVazia].ocupado = 1;
+
+	return 1;
+}
+
+//criar salas
+void criar_sala(SALAS* salas) {
+	//vamos pedir ao utilizador os dados para criar a nova sala
+				//declarecao de variaveis necessarias
+	char* codigo;
+	char* nome_sala;
+	int lotacao;
+	//vamos pedir o numero 
+	do
+	{
+		printf("Qual o numero da sala?\n");
+		scanf("%s", &codigo);
+	} while (strlen(codigo) == 0);
+
+	//vamos pedir o nome da sala 
+	do
+	{
+		printf("Qual o nome do sala?\n");
+		scanf("%s", &nome_sala);
+	} while (strlen(nome_sala) == 0);
+
+	// introducao da lotacao
+	
+		printf("Indique a lotacao da sala\n");
+		scanf("%d", &lotacao);
+
+	//valida se a sala já existe no nosso vector
+	//No caso de retornar 1 a UC ja existe, caso contrario podemos inserir 
+	if (valida_sala_existe_vector(salas, nome_sala) == 1) {
+		printf("A unidade curricular já existe!\n");
+	}
+	else {
+		if (insere_sala(salas, codigo, nome_sala, lotacao) == 1) {
+			printf("%s inserida com sucesso!\n", nome_sala);
+			printf("\n");
+		}
+		else {
+			printf("Ocorreu um erro ao guardar a sala em memoria\n");
+			printf("Tente novamente!\n");
+			printf("\n");
+		}
+	}
 }
 */
 
-//funcao principal do programa aluno.c
-void menu_alunos(ALUNOS* alunos, REGIMES* regimes, CURSO* cursos){
-    int escolha;
-    //Boas vindas ao usuario
-    printf("Bem vindo ao sistema de alunos do Politecnico de Viseu:\n");
-    //Criando loop de menu para o usuario
-    while (1) {
-        // apresentando opcoes
-        printf("\nSelecione sua opção:\n[1] Criar aluno:\n[2] Buscar Aluno:\n[3] Listar Alunos:\n[4] Sair\n");
-        scanf("%d", &escolha);
-        switch (escolha)
-        {
-        case 1://Criar aluno    
-            //alunos = fopen("alunos.txt", "a");//abre o arquivo com "A", cara criar caso necessario e escrever no final caso ja exista.
-            criar_aluno(alunos, regimes, cursos);
-            break;
-        case 2://Buscar Aluno
-            char numero[6];
-            printf("Digite o numero do Aluno:\n");
-            scanf("%s", &numero);
-            //buscar_aluno(numero);
-            break;
-        case 3: 
-            listar_alunos(alunos);
-            break;
-        case 4:
-            exit(0);
-        }
-    }
+//menu relativo as salas
+void menu_alunos(ALUNOS* alunos, REGIMES* regimes, CURSO* cursos) {
+    int opcao;
+
+	while (1) {
+		printf("\n");
+		printf("Bem-vindo ao menu das salas!\n");
+		printf("Escolha uma das opcoes:\n");
+		printf("1 - Consultar Alunos\n");
+		printf("2 - Adicionar Aluno\n");
+		printf("3 - Editar Aluno\n");
+		printf("4 - Apagar Aluno\n");
+		printf("0 - Sair\n");
+		printf("Introduza a Opcao: ");
+		scanf("%d", &opcao);
+		system("cls");
+		
+		switch (opcao) {
+		case 1: //lista as salas
+			listar_alunos(alunos);
+			break;
+		case 2: //cria alunos 
+			//criar_aluno(alunos);
+			break;
+		case 3: //Editar aluno 
+			break;
+		case 4: //Apagar aluno
+		case 0:
+			break;
+		}
+	}
+
+
 }
--- salas.c
+++ salas.c
@@ -14,7 +14,7 @@
 #define MAX_SALAS 100
 
 
-//passa para o vecto a informacao do ficheiro cursos
+//passa para o vecto a informacao do ficheiro salas.txt
 void import_txt_salas(SALAS* salas, STRING* V) {
 	int i = 0, k, n_campos_lidos;
 
@@ -50,7 +50,6 @@ void import_txt_salas(SALAS* salas, STRING* V) {
 
 }
 
-
 // funcao que lista as salas 
 void listar_salas(SALAS* salas) {
 	int i;
@@ -63,6 +62,95 @@ void listar_salas(SALAS* salas) {
 	}
 }
 
+//verifica se a sala ja existe
+int valida_sala_existe_vector(SALAS* salas, char* nome_sala) {
+	int i = 0; 
+	for ( i = 0; i < MAX_UNIDADES_CURRICULARES; i++)
+	{
+		if (salas[i].ocupado == 1) {
+			
+			if (strcmp(salas[i].nome_sala, nome_sala) == 0) { 
+				return 1;
+			}	
+		}
+	}
+	return 0;
+}
+
+//
+int get_posicao_vect_salas(SALAS* salas) {
+	int i;
+	for (i = 0; i < MAX_SALAS; i++)
+	{
+		if ((salas[i].ocupado == 0) && (i < MAX_SALAS)) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+//Insere sala
+int insere_sala(SALAS* salas, char* codigo, char* nome_sala, int lotacao) {
+	int posicaoVazia = 0;
+	posicaoVazia = get_posicao_vect_salas(salas);
+	if (posicaoVazia == -1) {
+		return -1;
+	}
+
+	//insere a nova sala no vector 
+	salas[posicaoVazia].codigo = codigo;
+	salas[posicaoVazia].nome_sala = nome_sala;
+	salas[posicaoVazia].lotacao = lotacao;
+	salas[posicaoVazia].ocupado = 1;
+
+	return 1;
+}
+
+//criar salas
+void criar_sala(SALAS* salas) {
+	//vamos pedir ao utilizador os dados para criar a nova sala
+				//declarecao de variaveis necessarias
+	char* codigo;
+	char* nome_sala;
+	int lotacao;
+	//vamos pedir o numero 
+	do
+	{
+		printf("Qual o numero da sala?\n");
+		scanf("%s", &codigo);
+	} while (strlen(codigo) == 0);
+
+	//vamos pedir o nome da sala 
+	do
+	{
+		printf("Qual o nome do sala?\n");
+		scanf("%s", &nome_sala);
+	} while (strlen(nome_sala) == 0);
+
+	// introducao da lotacao
+	
+		printf("Indique a lotacao da sala\n");
+		scanf("%d", &lotacao);
+
+	//valida se a sala já existe no nosso vector
+	//No caso de retornar 1 a UC ja existe, caso contrario podemos inserir 
+	if (valida_sala_existe_vector(salas, nome_sala) == 1) {
+		printf("A unidade curricular já existe!\n");
+	}
+	else {
+		if (insere_sala(salas, codigo, nome_sala, lotacao) == 1) {
+			printf("%s inserida com sucesso!\n", nome_sala);
+			printf("\n");
+		}
+		else {
+			printf("Ocorreu um erro ao guardar a sala em memoria\n");
+			printf("Tente novamente!\n");
+			printf("\n");
+		}
+	}
+}
+
+//menu relativo as salas
 void menu_salas(SALAS* salas) {
 	int opcao;
 
@@ -84,7 +172,7 @@ void menu_salas(SALAS* salas) {
 			listar_salas(salas);
 			break;
 		case 2: //cria salas 
-			
+			criar_sala(salas);
 			break;
 		case 3: //Editar salas 
 			break;
