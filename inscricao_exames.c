3d4789037aeebde7f0541e4048de97a340b81842
Pedro Rodrigues <phsr@live.com>
2023-01-08 19:39:31 +0000

    Add files via upload

--- inscricao_exames.c
+++ inscricao_exames.c
@@ -1,26 +1,61 @@
 #include <functions.h>
-#define MAX_ALUNOS 50
+#define MAX_ALUNOS 150
+
+//definicao da estrutura de exames
+typedef struct
+{
+	int codigo;
+	char* unidade_curricular;
+	int ano; 
+	char* curso;
+	char* epoca;
+	char* data;
+	char* hora;
+	int duracao;
+	char* sala;
+	char alunos_inscritos[150][6];
+	int realizado;
+	int ocupado;
+	int num_alunos;
+} EXAMES;
+
+//definicao da estrutura salas
+typedef struct{
+	int id;
+	char* codigo;
+	char* nome_sala;
+	int lotacao;
+	int ocupado;
+}SALAS;
 
 // Vincula um vetor de alunos a um exame
-void vincular_alunos_exame(char *alunos_ID[], int num_alunos, char *exame_ID, EXAMES *exames, int num_exames) {
-// Procure o exame pelo ID
-for (int i = 0; i < num_exames; i++) {
-if (strcmp(exames[i].codigo, exame_ID) == 0) {
-// Verifique se o exame já possui o máximo de alunos permitidos
-if (exames[i].num_alunos + num_alunos > MAX_ALUNOS) {
-printf("Não é possível vincular mais alunos ao exame, pois o limite máximo de alunos foi atingido.\n");
-return;
-}
-// Adicione os alunos ao vetor de alunos do exame
-for (int j = 0; j < num_alunos; j++) {
-strcpy(exames[i].alunos_inscritos[exames[i].num_alunos], alunos_ID[j]);
-exames[i].num_alunos++;
-}
-printf("Alunos vinculados ao exame com sucesso.\n");
-return;
-}
-}
-printf("Não foi encontrado nenhum exame com o ID especificado.\n");
+void vincular_alunos_exame(char *alunos_ID[], int num_alunos, char *exame_ID, EXAMES *exames, int num_exames, SALAS *salas, int num_salas) {
+  // Procure o exame pelo ID
+  for (int i = 0; i < num_exames; i++) {
+    if (strcmp(exames[i].codigo, exame_ID) == 0) {
+      // Verifique se a sala do exame possui lotação suficiente
+      int lotacao_disponivel = 0;
+      for (int j = 0; j < num_salas; j++) {
+        if (strcmp(exames[i].sala->codigo, salas[j].codigo) == 0) {
+        lotacao_disponivel = salas[j].lotacao - salas[j].ocupado;
+        break;
+        }
+      }
+      if (exames[i].num_alunos + num_alunos > lotacao_disponivel) {
+        printf("Não é possível vincular mais alunos ao exame, pois a sala não possui lotação suficiente.\n");
+        return;
+      }
+      // Adicione os alunos ao vetor de alunos do exame
+      for (int j = 0; j < num_alunos; j++) {
+        strcpy(exames[i].alunos_inscritos[exames[i].num_alunos], alunos_ID[j]);
+        exames[i].num_alunos++;
+        exames[i].sala->ocupado++;
+      }
+      printf("Alunos vinculados ao exame com sucesso.\n");
+      return;
+    }
+  }
+  printf("Não foi encontrado nenhum exame com o ID especificado.\n");
 }
 
 void desvincular_alunos_exame(char *alunos_ID[], int num_alunos, char *exame_ID, EXAMES *exames, int num_exames) {
